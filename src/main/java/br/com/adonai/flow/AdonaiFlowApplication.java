1c7c98a0df6987a7790b925fc4538040843dca6a
Geovane Araújo <geovane-araujo@outlook.com>
2022-10-24 22:06:43 -0300

    starter

---
+++ src/main/java/br/com/adonai/flow/AdonaiFlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.adonai.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdonaiFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdonaiFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/adonai/flow/AdonaiFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adonai.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdonaiFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
